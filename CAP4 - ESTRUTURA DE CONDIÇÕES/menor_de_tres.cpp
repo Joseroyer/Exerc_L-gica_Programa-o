c93c08cc7d9a5427ac26b10d29361f53b9331634
José Renato Royer <jrenato_royer@outlook.com>
2022-06-20 19:02:39 -0300

    up

--- CAP4 - ESTRUTURA DE CONDIÇÕES/menor_de_tres.cpp
+++ CAP4 - ESTRUTURA DE CONDIÇÕES/menor_de_tres.cpp
@@ -2,6 +2,23 @@
 #include <stdlib.h>
 
 int main(){
+	int n1,n2,n3,menor=0;
+	printf("PRIMEIRO VALOR:\n");
+	scanf("%d",&n1);
+	printf("SEGUNDO VALOR:\n");
+	scanf("%d",&n2);
+	printf("TERCEIRO VALOR:\n");
+	scanf("%d",&n3);
 	
+	if(n1<n2 && n1<n3)
+		menor = n1;
+	else{
+		if (n2<n1 && n2<n3)
+			menor = n2;
+		else{
+			menor = n3;
+		}
+	}
+	printf("MENOR = %d", menor);
 	return 0;
 }
