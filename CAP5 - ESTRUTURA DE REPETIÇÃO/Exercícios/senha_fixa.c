3a2e0b6212b2b595bb1d30afcf475041c2c3660d
José Renato Royer <jrenato_royer@outlook.com>
2022-06-21 20:33:20 -0300

    up

---
+++ CAP5 - ESTRUTURA DE REPETIÇÃO/Exercícios/senha_fixa.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int senha;
+    printf("Digite a senha: ");
+    scanf("%d", &senha);
+    while (senha != 2002)
+    {
+        printf("Senha Invalida! Tente novamente: ");
+        scanf("%d", &senha);
+    }
+    printf("Acesso permetido!");
+    return 0;
+}
