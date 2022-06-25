a2fa9079640f0d7b374509c1df4b10d85b5022ee
José Renato Royer <jrenato_royer@outlook.com>
2022-06-25 13:44:06 -0300

    up

---
+++ CAP5 - ESTRUTURA DE REPETIÇÃO/Exercícios/par_impar.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	int x,i,aux=0,number;
+	printf("Quantos numero voce vai digitar? ");
+	scanf("%d",&x);
+	for(i=0; i<=x; i++){
+		printf("Digite um numero\n");
+		scanf("%d",&number);
+		if(number==0){
+			printf("NULO\n");
+		}else{
+			if(number%2==0)
+			{
+				if(number>0)
+					printf("Par Positivo\n");
+				else
+					printf("Par Negativo\n");
+			}else{
+				if(number>0)
+					printf("Impar Positivo\n");
+				else
+					printf("Impar Negativo\n");
+			}
+		} 
+	}
+	return 0;
+}
