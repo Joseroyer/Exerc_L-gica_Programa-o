52afd45f2bce152638a7f629519eddd5a6b28682
José Renato Royer <jrenato_royer@outlook.com>
2022-07-02 17:24:56 -0300

    update exercício cada_linha
    problema com maior, estava pegando lixo

--- CAP7 - MATRIZ/Exercícios/cada_linha.cpp
+++ CAP7 - MATRIZ/Exercícios/cada_linha.cpp
@@ -3,7 +3,40 @@
 #define TF 4
 int main()
 {
-    
+    int j, i, Matriz[TF][TF], maior = 0;
+    for (i = 0; i < TF; i++)
+    {
+        for (j = 0; j < TF; j++)
+        {
+            printf("Elemento[%d,%d]:\n", i, j);
+            scanf("%d", &Matriz[i][j]);
+        }
+    }
+    for (i = 0; i < TF; i++)
+    {
+        for (j = 0; j < TF; j++)
+        {
+            printf("%d ",Matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("MAIOR ELEMENTO DE CADA LINHA: \n");
+    for (i = 0; i < TF; i++)
+    {
+        maior=0;
+        for (j = 0; j < TF; j++)
+        {
+            if (Matriz[i][j] > maior)
+                maior = Matriz[i][j];
+        }
+        printf("%d\n",maior);
+    }
+
+    // for (i = 0; i < TF; i++)
+    // {
+    //     printf("%d \n", vetMenor[i]);
+    // }
 
     return 0;
 }
