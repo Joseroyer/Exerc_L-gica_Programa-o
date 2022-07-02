493d82ac09ca0fbc55f5865f13d04faa183c4cb2
José Renato Royer <jrenato_royer@outlook.com>
2022-07-02 16:41:56 -0300

    Update diagonal_negativos.cpp

--- CAP7 - MATRIZ/Exercícios/diagonal_negativos.cpp
+++ CAP7 - MATRIZ/Exercícios/diagonal_negativos.cpp
@@ -6,24 +6,30 @@ int main()
     int N, i, j, Matriz[TF][TF], contN = 0;
     // printf("Qual a ordem da matriz?");
     // scanf("%d", &N);
-    for (i = 0; i < TF; i++)
-        for (j = 0; j < TF; j++)
-        {
-            printf("Elemento[%d,%d]:\n", i, j);
-            scanf("%d", &Matriz[i][j]);
-        }
-    for (i = 0; i < TF; i++)
-        for (j = 0; j < TF; j++)
-        {
-            if (Matriz[i][j] < 0)
-                contN++;
-        }
-
-    printf("DIAGONAL PRINCIPAL: \n");
-    for (i = 0; i < TF; i++)
+    if (N > 0 && N <= 10)
     {
-        printf("%d \t", Matriz[i][i]);
+        for (i = 0; i < TF; i++)
+            for (j = 0; j < TF; j++)
+            {
+                printf("Elemento[%d,%d]:\n", i, j);
+                scanf("%d", &Matriz[i][j]);
+            }
+        for (i = 0; i < TF; i++)
+            for (j = 0; j < TF; j++)
+            {
+                if (Matriz[i][j] < 0)
+                    contN++;
+            }
+
+        printf("DIAGONAL PRINCIPAL: \n");
+        for (i = 0; i < TF; i++)
+        {
+            printf("%d \t", Matriz[i][i]);
+        }
+        printf("\nQUANTIDADE DE NEGATIVOS: %d", contN);
     }
-    printf("\nQUANTIDADE DE NEGATIVOS: %d", contN);
+    else
+        printf("Digite um valor correto!!");
+
     return 0;
 }
