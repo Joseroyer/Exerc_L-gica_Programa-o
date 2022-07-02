5f0b147ef2435589aaee4bd94faff295b147d526
José Renato Royer <jrenato_royer@outlook.com>
2022-07-02 16:39:15 -0300

    up

---
+++ CAP7 - MATRIZ/Exercícios/diagonal_negativos.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TF 3
+int main()
+{
+    int N, i, j, Matriz[TF][TF], contN = 0;
+    // printf("Qual a ordem da matriz?");
+    // scanf("%d", &N);
+    for (i = 0; i < TF; i++)
+        for (j = 0; j < TF; j++)
+        {
+            printf("Elemento[%d,%d]:\n", i, j);
+            scanf("%d", &Matriz[i][j]);
+        }
+    for (i = 0; i < TF; i++)
+        for (j = 0; j < TF; j++)
+        {
+            if (Matriz[i][j] < 0)
+                contN++;
+        }
+
+    printf("DIAGONAL PRINCIPAL: \n");
+    for (i = 0; i < TF; i++)
+    {
+        printf("%d \t", Matriz[i][i]);
+    }
+    printf("\nQUANTIDADE DE NEGATIVOS: %d", contN);
+    return 0;
+}
